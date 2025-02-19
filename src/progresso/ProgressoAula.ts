49db8cf89a984e43351460c5cf6649bda0bd0dff
flavioasp77 <flavioasp77@gmail.com>
2025-02-19 19:37:43 -0300

    implementa a entidade ProgressoAula

---
+++ src/progresso/ProgressoAula.ts
@@ -0,0 +1,58 @@
+import Erros from "@/constants/Erros";
+import ErroValidacao from "@/error/ErroValidacao";
+import Duracao from "@/shared/Duracao";
+import Entidade, { EntidadeProps } from "@/shared/Entidade";
+import NomeSimples from "@/shared/NomeSimples";
+
+export interface ProgressoAulaProps extends EntidadeProps {
+  nomeAula?: string;
+  duracao?: number;
+  dataInicio?: Date;
+  dataConclusao?: Date;
+}
+
+export default class ProgressoAula extends Entidade<
+  ProgressoAula,
+  ProgressoAulaProps
+> {
+  readonly nomeAula: NomeSimples;
+  readonly duracao: Duracao;
+  readonly dataInicio?: Date;
+  readonly dataConlusao?: Date;
+
+  constructor(props: ProgressoAulaProps) {
+    super(props);
+    this.nomeAula = new NomeSimples(props.nomeAula!, 3, 50);
+    this.duracao = new Duracao(props.duracao!);
+    this.dataInicio = props.dataInicio;
+    this.dataConlusao = props.dataConclusao;
+
+    if (!props.id) {
+      ErroValidacao.lancar(Erros.ID_INVALIDO, props.id);
+    }
+
+    if (this.duracao.zerada) {
+      ErroValidacao.lancar(Erros.DURACAO_ZERADA);
+    }
+  }
+
+  get iniciado(): boolean {
+    return this.dataInicio !== null;
+  }
+
+  get concluido(): boolean {
+    return this.dataConlusao !== null;
+  }
+
+  iniciar(): ProgressoAula {
+    return this.clone({ dataInicio: new Date() });
+  }
+
+  concluir(): ProgressoAula {
+    return this.clone({ dataConclusao: new Date() });
+  }
+
+  zerar(): ProgressoAula {
+    return this.clone({ dataConclusao: undefined });
+  }
+}
